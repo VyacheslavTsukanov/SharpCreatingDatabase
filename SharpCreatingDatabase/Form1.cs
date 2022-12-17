fef6311a4bde1570fc1197e76bc46de4d69f8d13
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2022-12-17 19:23:37 +0400

    Connecting the database

--- SharpCreatingDatabase/Form1.cs
+++ SharpCreatingDatabase/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace SharpCreatingDatabase
 {
     public partial class Form1 : Form
     {
+        private SqlConnection sqlConnection = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+
+            sqlConnection.Open();
+            
+            if(sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключено!!!");
+            }
         }
     }
 }
